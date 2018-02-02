e5fdb51c0eb0312275fe327a220f16aeb38f645b
Matthias Klan <mk@viewar.com>
2018-02-02 16:32:33 +0100

    add initial blue ball back

--- src/views/init/init.view.js
+++ src/views/init/init.view.js
@@ -38,6 +38,11 @@ export default compose(
 
       });
 
+      await setTimeout(async () => {
+        const model = modelManager.findModelByForeignKey('ball');
+        await sceneManager.insertModel(model, { pose: { position: { x: 0, y: 0, z: 0 }} } );
+      }, 0);
+
       socketConnection.socket.on('connect_error', () => {
         alert('Socket Connection Error!');
       });
